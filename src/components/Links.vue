4eef21fc1f768c2373928a9e5590729fb8bb02e6
BeianYu <162873469+BeianYu@users.noreply.github.com>
2024-07-10 18:21:05 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Amilia, BookReader, CaretSquareRight, File } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Amilia, BookReader, CaretSquareRight, File, Download, Images, PencilAlt } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper";
@@ -74,6 +74,9 @@ const siteIcon = {
   BookReader,
   CaretSquareRight,
   File,
+  Download,
+  PencilAlt,
+  Images,
 };
 
 // 链接跳转
